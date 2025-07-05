944f30bb6970e003cbffa8f08586cb6ea51776cc
dfonsec <dfonseca@hmc.edu>
2025-07-05 00:16:47 -0400

    Refactored to incorporate inheritance to new schema

--- app/schemas.py
+++ app/schemas.py
@@ -2,7 +2,11 @@
 # We use the BaseModel class and pass it into the
 # Schema class so it can inherit the BaseModel's attributes
 from pydantic import BaseModel
-class Post(BaseModel):
+class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
+    
+class PostCreate(PostBase):
+    pass
+
