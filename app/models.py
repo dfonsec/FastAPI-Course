e691798179f6f46524884f48e6aac75a79e98a4e
dfonsec <dfonseca@hmc.edu>
2025-07-05 00:01:48 -0400

    Refactored everything to incorporate Alchemy approach

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, func
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class Post(Base):
     __tablename__ = "posts"
@@ -7,5 +9,6 @@ class Post(Base):
     id  = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, nullable=True)
+    published = Column(Boolean, server_default='TRUE', nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
     
