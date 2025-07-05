136cad46f4c0f7ebf1593d032e378edcd11538f7
dfonsec <dfonseca@hmc.edu>
2025-07-05 00:01:31 -0400

    Refactored everything to incorporate Alchemy approach

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
 import psycopg
 from psycopg.rows import dict_row
+from . import models
+from .database import engine, SessionLocal, get_db
 import time
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 # Pydantic Schema
 # We use the BaseModel class and pass it into the
@@ -16,7 +22,6 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 
 while True: 
     try:
@@ -44,30 +49,42 @@ my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1
 def main():
     return {"message": "Welcome to my "}
 
+# @app.get("/sqlalchemy")
+# def test_posts(db: Session = Depends(get_db)):
+#     posts = db.query(models.Post).all()
+#     return {"data": posts}
+
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 
 # body will extract all the fields from the body and
 # then convert it into a python dictionary
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_post(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     
     return {"data": new_post}
 
 # The ID field is a path parameter
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
+def get_post(id: int, db: Session = Depends(get_db)):
     
-    cursor.execute(f"""SELECT * FROM posts WHERE id={id}""")
-    post = cursor.fetchone()
+    # cursor.execute("""SELECT * FROM posts WHERE id= %s """, (str(id), ))
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found.")
             
@@ -75,24 +92,36 @@ def get_post(id: int, response: Response):
 
 
 @app.delete("/posts/{id}")
-def delete_post(id: int, status_code=status.HTTP_204_NO_CONTENT):
-    cursor.execute(f"""DELETE FROM posts WHERE id={id}""")
-    
-
-    my_posts.pop(post_idx)
-    # 204 you don't want to send any data back
+def delete_post(id: int, db: Session = Depends(get_db), status_code=status.HTTP_204_NO_CONTENT):
+    # cursor.execute("""DELETE FROM posts WHERE id= %s RETURNING *""", (str(id), ))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if not post.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found.")
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    post_idx = find_index_post(id)
+def update_post(id: int, post: Post,  db: Session = Depends(get_db)):
+    # cursor.execute(f"""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id),))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
     
-    if not post_idx:
+    # Define quert 
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    
+    # Grab post
+    post_found = post_query.first()
+    
+    if not post_found:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} does not exist')
     
-    post_dict = post.model_dump()
-    post_dict["id"] = id
-    my_posts[post_idx] = post_dict
     
-    return {"data": post_dict}
+    post_query.update(post.model_dump(), synchronize_session=False)
+    
+    db.commit()
+    
+    return {"data": post_query.first()}
     
