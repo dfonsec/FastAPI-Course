b5eb6655ba714628b5df64e23267d9708df83a5d
dfonsec <dfonseca@hmc.edu>
2025-07-06 19:05:55 -0400

    Saving work

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional, List
-from random import randrange
+from fastapi import FastAPI
 import psycopg
 from psycopg.rows import dict_row
-from . import models, schemas
-from .database import engine, SessionLocal, get_db
+from . import models
+from .database import engine
 import time
 from sqlalchemy.orm import Session
-from .schemas import PostBase, PostCreate, UserOut
-from .utils import hash
+
+from .routers import post, user
 
 
 # Telling passlib to use the bcrypt algorithm to encrypt passwords
@@ -32,118 +28,15 @@ while True:
         print("Error was:", error)
         time.sleep(2)
     
+    
+app.include_router(post.router)
+app.include_router(user.router)    
 # This is a path operation
 # The / is just the root path operation
 @app.get("/")
 def main():
     return {"message": "Welcome to my "}
 
-# @app.get("/sqlalchemy")
-# def test_posts(db: Session = Depends(get_db)):
-#     posts = db.query(models.Post).all()
-#     return {"data": posts}
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
-    return posts
 
 
-# body will extract all the fields from the body and
-# then convert it into a python dictionary
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_post(post: PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
-    #                (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    new_post = models.Post(**post.model_dump())
-    db.add(new_post)
-    db.commit()
-    
-    # refresh takes the entry that was created in the db and updates the python object data to reflect what's in the db
-    db.refresh(new_post)
-    
-    return new_post
 
-# The ID field is a path parameter
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
-    
-    # cursor.execute("""SELECT * FROM posts WHERE id= %s """, (str(id), ))
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found.")
-            
-    return post
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id= %s RETURNING *""", (str(id), ))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if not post.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found.")
-    post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}", response_model=schemas.Post)
-def update_post(id: int, post: PostBase,  db: Session = Depends(get_db)):
-    # cursor.execute(f"""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id),))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-    
-    # Define query
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    
-    # Grab post
-    post_found = post_query.first()
-    
-    if not post_found:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} does not exist')
-    
-    
-    post_query.update(post.model_dump(), synchronize_session=False)
-    
-    db.commit()
-    
-    return post_query.first()
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
-    #                (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    
-    
-    # Hash the password - user.password
-    hashed_password = hash(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    
-    # refresh takes the entry that was created in the db and updates the python object data to reflect what's in the db
-    db.refresh(new_user)
-    
-    return new_user
-    
-@app.get("/users/{id}", response_model=UserOut)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id: {id} does not exist')
-    
-    
-    return user
-    
