60c0deaac06f01d5f04b7edf736c90dfc98547e4
dfonsec <dfonseca@hmc.edu>
2025-07-06 18:12:18 -0400

    Updated main to include user path operationis:

--- app/main.py
+++ app/main.py
@@ -9,8 +9,11 @@ from . import models, schemas
 from .database import engine, SessionLocal, get_db
 import time
 from sqlalchemy.orm import Session
-from .schemas import PostBase, PostCreate
+from .schemas import PostBase, PostCreate, UserOut
+from .utils import hash
 
+
+# Telling passlib to use the bcrypt algorithm to encrypt passwords
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -40,8 +43,8 @@ def main():
 #     posts = db.query(models.Post).all()
 #     return {"data": posts}
 
-@app.get("/posts")
-def get_posts(db: Session = Depends(get_db), response_model=List[schemas.Post]):
+@app.get("/posts", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
@@ -66,8 +69,8 @@ def create_post(post: PostCreate, db: Session = Depends(get_db)):
     return new_post
 
 # The ID field is a path parameter
-@app.get("/posts/{id}")
-def get_post(id: int, db: Session = Depends(get_db), response_model=schemas.Post):
+@app.get("/posts/{id}", response_model=schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db)):
     
     # cursor.execute("""SELECT * FROM posts WHERE id= %s """, (str(id), ))
     # post = cursor.fetchone()
@@ -79,8 +82,8 @@ def get_post(id: int, db: Session = Depends(get_db), response_model=schemas.Post
     return post
 
 
-@app.delete("/posts/{id}")
-def delete_post(id: int, db: Session = Depends(get_db), status_code=status.HTTP_204_NO_CONTENT):
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""DELETE FROM posts WHERE id= %s RETURNING *""", (str(id), ))
     # deleted_post = cursor.fetchone()
     # conn.commit()
@@ -91,8 +94,8 @@ def delete_post(id: int, db: Session = Depends(get_db), status_code=status.HTTP_
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: PostBase,  db: Session = Depends(get_db), response_model=schemas.Post):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, post: PostBase,  db: Session = Depends(get_db)):
     # cursor.execute(f"""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id),))
     # updated_post = cursor.fetchone()
     # conn.commit()
@@ -112,4 +115,35 @@ def update_post(id: int, post: PostBase,  db: Session = Depends(get_db), respons
     db.commit()
     
     return post_query.first()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    
+    
+    # Hash the password - user.password
+    hashed_password = hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    
+    # refresh takes the entry that was created in the db and updates the python object data to reflect what's in the db
+    db.refresh(new_user)
+    
+    return new_user
+    
+@app.get("/users/{id}", response_model=UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id: {id} does not exist')
+    
+    
+    return user
     
